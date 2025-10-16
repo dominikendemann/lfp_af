f88048eb5552b9d2230f41536f21c6f30a95947b
Dominik Endemann <189213655+dominikendemann@users.noreply.github.com>
2025-10-16 11:26:03 +0200

    Add files via upload

---
+++ app.R
@@ -0,0 +1,58 @@
+# Package setup ---------------------------------------------------------------
+
+# Install required packages:
+# install.packages("pak")
+# pak::pak("surveydown-dev/surveydown") # Development version from GitHub
+
+# Load packages
+library(surveydown)
+
+
+# Database setup --------------------------------------------------------------
+# en5M1K8JZQmJqa9u
+
+# Details at: https://surveydown.org/docs/storing-data
+#
+# surveydown stores data on any PostgreSQL database. We recommend
+# https://supabase.com/ for a free and easy to use service.
+#
+# Once you have your database ready, run the following function to store your
+# database configuration parameters in a local .env file:
+#
+# sd_db_config()
+#
+# Once your parameters are stored, you are ready to connect to your database.
+# For this demo, we set ignore = TRUE in the following code, which will ignore
+# the connection settings and won't attempt to connect to the database. This is
+# helpful if you don't want to record testing data in the database table while
+# doing local testing. Once you're ready to collect survey responses, set
+# ignore = FALSE or just delete this argument.
+
+db <- sd_db_connect(ignore = F)
+
+# UI setup --------------------------------------------------------------------
+
+ui <- sd_ui()
+
+# Server setup ----------------------------------------------------------------
+
+server <- function(input, output, session) {
+
+  # Define any conditional skip logic here (skip to page if a condition is true)
+  sd_skip_forward()
+
+  # Define any conditional display logic here (show a question if a condition is true)
+ sd_show_if(
+    input$tandem == "ja" ~ "name_tandem" 
+  )
+
+  # Database designation and other settings
+  sd_server(
+    db = db,
+    required_questions = c("name1", "name2", "seminar", "klassenstufe", "erhebung", "tandem")
+  )
+
+}
+
+# Launch the app
+shiny::shinyApp(ui = ui, server = server)
